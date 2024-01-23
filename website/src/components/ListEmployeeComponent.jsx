1bd50f1bfd3d8e43515ae6ca357614e5840e8612
hellowgh <hellowgh@qq.com>
2024-01-23 22:28:17 +0800

    完成删除功能

--- website/src/components/ListEmployeeComponent.jsx
+++ website/src/components/ListEmployeeComponent.jsx
@@ -1,38 +1,51 @@
 import { useEffect, useState } from 'react';
-import { listEmployees } from '../service/employeeService.js';
+import { deleteEmployee, listEmployees } from '../service/employeeService.js';
 import { Button, Table } from 'antd';
 import { useNavigate } from 'react-router-dom';
 
-const columns = [
-  {
-    title: 'Employ Id',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: 'First Name',
-    dataIndex: 'firstName',
-    key: 'firstName',
-  },
-  {
-    title: 'Last Name',
-    dataIndex: 'lastName',
-    key: 'lastName',
-  },
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    key: 'email',
-  },
-  {
-    title: 'Actions',
-    dataIndex: '',
-    key: '',
-  },
-];
-
 const ListEmployeeComponent = () => {
   const [employees, setEmployees] = useState([]);
+
+  const removeEmployee = async (obj) => {
+    const { id } = obj;
+
+    await deleteEmployee(id);
+    await getAllEmployees();
+  };
+
+  const columns = [
+    {
+      title: 'Employ Id',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'First Name',
+      dataIndex: 'firstName',
+      key: 'firstName',
+    },
+    {
+      title: 'Last Name',
+      dataIndex: 'lastName',
+      key: 'lastName',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Actions',
+      dataIndex: '',
+      key: '',
+      render: (record) => (
+        <>
+          <Button type={'primary'} className="mr-20">Update</Button>
+          <Button onClick={() => removeEmployee(record)} danger>Delete</Button>
+        </>
+      ),
+    },
+  ];
 
   const getAllEmployees = async () => {
     setEmployees([]);
